41609897e427b09dbe937ef82b85b9ed246ea248
takayuki yoshizawa <yunisuke00@gmail.com>
2019-05-27 22:19:50 +0900

    プロジェクト

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    void Update () {
+        if (Input.GetMouseButtonDown (0)) {
+            if (_rigidbody == null) _rigidbody = GetComponent<Rigidbody2D>();
+            _rigidbody.velocity = new Vector3 (0f, 8f, 0f);
+        }
+        if (Input.GetKey (KeyCode.Space)) {
+            if (_rigidbody == null) _rigidbody = GetComponent<Rigidbody2D>();
+            _rigidbody.velocity = new Vector3 (0f, -0.5f, 0f);
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D collider) {
+        Debug.Log ("Hit!");
+    }
+
+    private void Move () {
+
+    }
+}
